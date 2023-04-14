dc7c8e1607a38b5a11f75c09e451e26500719927
slivcaigor <slivcaigor@gmail.com>
2023-04-14 15:03:51 +0200

    Add project files.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        // Proprietà in solo lettura
+        public string Name { get; }
+        public string Surname { get; }
+        public string Street { get; }
+        public string City { get; }
+        public string Province { get; }
+        public string ZIP { get; }
+
+        // Costruttore
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            // Assegna i valori delle proprietà con i parametri del costruttore
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zip;
+        }
+    }
+}
